13313d1f92e0e713641b77c0816958255e70dd78
George Fountain <gfountain257@gmail.com>
2025-07-10 09:14:01 -0400

    Confirm Tailwind globals setup

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-white text-black">{children}</body>
+      <body suppressHydrationWarning={true} className="bg-white text-black">
+        {children}
+      </body>
     </html>
   );
 }
+
